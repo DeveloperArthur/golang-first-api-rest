7bee395313bf59e8d624fcbdb90da5fa53c993f4
Arthur Dos Santos Almeida <arthur.almeida@g.globo>
2023-06-15 12:21:02 -0300

    refactor: migrando endpoint de saudação para um controller proprio

--- controllers/hello_controller.go
+++ controllers/hello_controller.go
@@ -1,1 +1,14 @@
 package controllers
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func Saldacao(c *gin.Context) {
+	nome := c.Params.ByName("nome")
+	fmt.Println(nome)
+	c.JSON(200, gin.H{
+		"APi diz:": "E ai " + nome + " tudo beleza?",
+	})
+}
