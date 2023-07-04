a184297451d12ae393d9f1ddaca60af7ee4ee93e
Arthur Dos Santos Almeida <arthur.almeida@g.globo>
2023-07-04 18:09:36 -0300

    feat: setando variavel de ambiente

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"golang-first-api-rest/database"
 	"golang-first-api-rest/routes"
+	"os"
 )
 
 func main() {
+	os.Setenv("VIACEP_ENDPOINT", "https://viacep.com.br/ws/")
 	database.ConectaComBancoDeDados()
 	routes.HandleRequests()
 }
